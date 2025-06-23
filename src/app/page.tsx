b00af4081816f8921732598975d4268acf2df794
Eli Nygård <eli.nygard@gmail.com>
2025-06-23 23:02:39 +0200

    Update page.tsx
    Edit text content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className="font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-2xl md:text-3xl mb-4">Et lite småbruk som dyrker trivsel for alle</h1>
-      <p>Revhaugen Gård er godkjent Inn på tunet-gård, og tilbyr kvalitetsikrede velferdstjenester som gir mestring, utvikling og trivsel.</p>
+      <p>Revhaugen Gård tilbyr kvalitetsikrede velferdstjenester som gir mestring, utvikling og trivsel.</p>
     </section>
   );
 }
