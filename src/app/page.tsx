440ae58506db6aa71f38f218f74c00116828fe96
Eli Nygård <eli.nygard@gmail.com>
2025-06-25 19:52:36 +0200

    Update page.tsx
    Adds a welcome text, adds h2.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <section className="font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-2xl md:text-3xl mb-4">Et lite småbruk som dyrker trivsel for alle</h1>
-      <p>Revhaugen Gård tilbyr kvalitetsikrede velferdstjenester som gir mestring, utvikling og trivsel.</p>
+      <h1 className="text-2xl md:text-3xl mb-4">Velkommen til Revhaugen gård</h1>
+
+<h2>Et lite småbruk som dyrker trivsel for alle</h2>
+      <p>Her kommer det snart mer info om Revhaugen Gård. Vi ønsker å tilby kvalitetsikrede velferdstjenester som gir mestring, utvikling og trivsel.</p>
     </section>
   );
 }
